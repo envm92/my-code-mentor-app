aac7a084f288f1544e41dbd1dc2df8e2d3d8b0e3
Erika Valdes <envm92@gmail.com>
2024-07-31 15:54:54 -0700

    Edited Readme

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # MyCodeMentorApp
+![App ScreenShoot](example.jpeg)
+
+This mentor code application, developed using Angular, connects to a WebSocket service. You can find the server [here](https://github.com/envm92/mentor-code-openai-nodejs). The purpose of this application is to explain a coding solution by providing a description.
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.1.2.
 
@@ -6,22 +9,6 @@ This project was generated with [Angular CLI](https://github.com/angular/angular
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
 
-## Code scaffolding
+## Styling with 98.css
 
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
-
-## Build
-
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
-
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To use this command, you need to first add a package that implements end-to-end testing capabilities.
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.dev/tools/cli) page.
+I've used the [98.css](https://github.com/jdan/98.css) library to style my project. It provides a nostalgic Windows 98 aesthetic. Check it out!
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatButtonModule } from '@angular/material/button';
-import { HttpClient } from '@angular/common/http';
-import { MatInputModule } from '@angular/material/input';
-import {MatDividerModule} from '@angular/material/divider';
 import { FormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -14,74 +9,102 @@ import { CommonModule } from '@angular/common';
   imports: [
     RouterOutlet,
     CommonModule,
-    MatButtonModule,
-    MatInputModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatDividerModule,
     FormsModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-code-mentor-app';
   ws: any = null;
   responseOpenAi = '';
   threadId = '';
-  message = '';
   description = '';
   solution = '';
+  responseLinesCount = 0;
 
-  constructor(private http: HttpClient) {}
-
-  showMessage(message: any) {
-    console.log(message);
+  public getScreenHeight: any;
+  rows = 100;
+  statusEnum: any = {
+    0 : 'Loading...',
+    1 : 'Stand by',
+    2 : 'Success Response', 
+    3 : 'Error !'
+  };
+  status = 0;
+  ngOnInit() {
+    this.getScreenHeight = window.innerHeight;
+    this.rows = (this.getScreenHeight/12) -10;
+    this.openWebSocket();
   }
 
   handleResponse(response:any) {
-    console.log(response);
     return response.ok
       ? response.json().then((data: any) => JSON.stringify(data, null, 2))
       : Promise.reject(new Error('Unexpected response'));
+  }
+
+  resetConection() {
+    if(this.ws) {
+      this.ws.close();
+    }
+    this.description = '';
+    this.solution = '';
+    this.responseOpenAi = '';
+    this.responseLinesCount =0;
+    this.openWebSocket();
   }
   openWebSocket() {
     if (this.ws) {
       this.ws.onerror = this.ws.onopen = this.ws.onclose = null;
       this.ws.close();
+      this.status = 3;
     }
 
     this.ws = new WebSocket(`ws://localhost:8080/`);
     this.ws.onerror = () =>{
-      this.showMessage('WebSocket error');
+      this.status = 3;
     };
     this.ws.onopen = () => {
-      this.showMessage('WebSocket connection established');
+      this.status = 1;
     };
     this.ws.onmessage = (message: any) => {
       const res = JSON.parse(message.data);
       if (res.thread_id) {
         this.threadId = res.thread_id;
       } else {
-        this.responseOpenAi += res.text;
+        if (res.running) {
+          this.responseOpenAi += res.text;
+          if (/\r|\n/.exec(res.text)) {
+            this.responseLinesCount++;
+          }
+        } else {
+          this.status = 2;
+        }
       }
     };
     this.ws.onclose = () => {
-      this.showMessage('WebSocket connection closed');
       this.ws = null;
     };
   }
   sendMessage() {
     if (!this.ws) {
-      this.showMessage('No WebSocket connection');
+      this.status = 3;
       return;
     }
     const runThreadReq = {
       action: 'add_message',
-      assistant_id: 'asst_W96G4YNrgAyBeRWcgA1Lgt0f',
+      assistant_id: '${{YOUR_ASSISTANT_ID}}',
       thread_id: this.threadId,
-      message: this.message
+      message: `Help me to explan this code solution, pleas return the same solution with the explanation in comments,giving this description: ${this.description} <--- END DESCRIPTION --> and this is my solution: ${this.solution}`
     }
     this.ws.send(JSON.stringify(runThreadReq));
+    this.status = 0;
+  }
+
+  @HostListener('window:resize', ['$event'])
+
+  onWindowResize() {
+    this.getScreenHeight = window.innerHeight;
+    this.rows = (this.getScreenHeight/12) -10;
   }
 }
